64c8cb2a25884e1266647e9e1f15bcd38b1deb83
Lightweight Mobile NoSQL Database <iboxdb@users.noreply.github.com>
2016-06-04 20:04:45 -0700

    m2

--- nname/nname/Program.cs
+++ nname/nname/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
- 
+
+using dnlib.DotNet;
 
 namespace nname
 {
@@ -8,6 +9,15 @@ namespace nname
     {
         static void Main(string[] args)
         {
+            var path = args[0];
+            var pathfix = path.Substring(0, path.Length - 4) + "_fixed" + path.Substring(path.Length - 4);
+
+            ModuleDefMD module = ModuleDefMD.Load(path);
+
+
+            Console.WriteLine(path + " -> " + pathfix + " isILOnly=" + module.IsILOnly);
+            module.Write(pathfix);
+
             Console.WriteLine(typeof(dnlib.DotNet.ArraySig));
         }
     }
